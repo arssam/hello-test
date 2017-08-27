71be70afc78006ba1d165cb1bdb5fa6aa836dcf4
sam <327156521@qq.com>
2017-08-27 16:30:35 +0800

    项目初始化

--- hello.js
+++ hello.js
@@ -1,7 +1,12 @@
-module.exports = function (...rest) {
-    var sum = 0;
-    for (let n of rest) {
-        sum += n;
-    }
-    return sum;
-};
+console.log('init hello.js...');
+
+const fs = require('mz/fs');
+
+module.exports = async () => {
+	const expression = await fs.readFile('./data.txt', 'utf-8');
+	const fn = new Function('return ' + expression);
+	const r = fn();
+	console.log(`Calculate: ${expression} = ${r}`);
+	return r;
+}
+
